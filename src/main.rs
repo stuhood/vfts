88037b15192d4e9486cc85d1a9983bd27e4a8e48
Stu Hood <stuhood@gmail.com>
2025-04-26 11:30:32 -0700

    Working.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use std::sync::Arc;
 use datafusion::datasource::listing::{
     ListingOptions, ListingTable, ListingTableConfig, ListingTableUrl,
 };
-use datafusion::prelude::SessionContext;
-use object_store::ObjectStore;
-use object_store::local::LocalFileSystem;
+use datafusion::execution::SessionStateBuilder;
+use datafusion::prelude::{SessionConfig, SessionContext};
 use tempfile::tempdir;
 use tokio::fs::OpenOptions;
-use url::Url;
-use vortex_array::arrays::{ChunkedArray, StructArray, VarBinArray};
+use vortex_array::arrays::{BooleanBuffer, ChunkedArray, StructArray, VarBinArray};
 use vortex_array::stream::ArrayStreamArrayExt;
 use vortex_array::validity::Validity;
 use vortex_array::{Array, IntoArray};
@@ -21,26 +19,32 @@ use vortex_file::VortexWriteOptions;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let temp_dir = tempdir()?;
-    let strings = ChunkedArray::from_iter([
-        VarBinArray::from(vec!["ab", "foo", "bar", "baz"]).into_array(),
-        VarBinArray::from(vec!["ab", "foo", "bar", "baz"]).into_array(),
+    let key = ChunkedArray::from_iter([
+        buffer![0u64, 1, 2, 3].into_array(),
+        buffer![4u64, 5, 6, 7].into_array(),
     ])
     .into_array();
 
-    let numbers = ChunkedArray::from_iter([
-        buffer![1u32, 2, 3, 4].into_array(),
-        buffer![5u32, 6, 7, 8].into_array(),
+    let token = ChunkedArray::from_iter([
+        VarBinArray::from(vec!["ab", "foo", "bar", "foo"]).into_array(),
+        VarBinArray::from(vec!["biz", "boz", "boop", "blep"]).into_array(),
+    ])
+    .into_array();
+
+    let bit = ChunkedArray::from_iter([
+        BooleanBuffer::from(vec![true, false, true, true]).into_array(),
+        BooleanBuffer::from(vec![true, false, true, false]).into_array(),
     ])
     .into_array();
 
     let st = StructArray::try_new(
-        ["strings".into(), "numbers".into()].into(),
-        vec![strings, numbers],
+        ["key".into(), "token".into(), "bit".into()].into(),
+        vec![key, token, bit],
         8,
         Validity::NonNullable,
     )?;
 
-    let filepath = temp_dir.path().join("a.vtx");
+    let filepath = temp_dir.path().join("a.vortex");
 
     let f = OpenOptions::new()
         .write(true)
@@ -53,11 +57,10 @@ async fn main() -> anyhow::Result<()> {
         .write(f, st.to_array_stream())
         .await?;
 
-    let ctx = SessionContext::new();
-
-    let object_store: Arc<dyn ObjectStore> = Arc::new(LocalFileSystem::new());
-    let url: Url = Url::try_from("file://")?;
-    ctx.register_object_store(&url, object_store);
+    let ctx: SessionContext = SessionStateBuilder::new()
+        .with_config(SessionConfig::new().set_bool("datafusion.catalog.information_schema", true))
+        .build()
+        .into();
 
     let format = Arc::new(VortexFormat::default());
     let table_url = ListingTableUrl::parse(
@@ -66,15 +69,15 @@ async fn main() -> anyhow::Result<()> {
             .ok_or_else(|| vortex_err!("Path is not valid UTF-8"))?,
     )?;
     let config = ListingTableConfig::new(table_url)
-        .with_listing_options(ListingOptions::new(format as _))
+        .with_listing_options(ListingOptions::new(format))
         .infer_schema(&ctx.state())
         .await?;
 
     let listing_table = Arc::new(ListingTable::try_new(config)?);
 
-    ctx.register_table("vortex_tbl", listing_table as _)?;
+    ctx.register_table("vortex_tbl", listing_table)?;
 
-    run_query(&ctx, "SELECT * FROM vortex_tbl").await?;
+    run_query(&ctx, "SELECT * FROM vortex_tbl WHERE bit = true AND token = 'foo'").await?;
 
     Ok(())
 }
@@ -82,7 +85,7 @@ async fn main() -> anyhow::Result<()> {
 async fn run_query(ctx: &SessionContext, query_string: impl AsRef<str>) -> anyhow::Result<()> {
     let query_string = query_string.as_ref();
 
-    ctx.sql(&format!("EXPLAIN {query_string}"))
+    ctx.sql(&format!("EXPLAIN ANALYZE {query_string}"))
         .await?
         .show()
         .await?;
