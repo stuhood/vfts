b5c6c45ed7ff9a69a6de4a774dc3cfd6a0530b6a
Stu Hood <stuhood@gmail.com>
2025-04-27 23:11:44 -0700

    Working end to end.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::sync::Arc;
 
-use vortex_array::IntoArray;
-use vortex_array::arrays::{ListArray, StructArray};
-use vortex_array::validity::Validity;
+use vortex_array::arrays::{BooleanBufferBuilder, ConstantArray, ListArray};
+use vortex_array::compute;
+use vortex_array::{Array, ArrayRef, IntoArray, ToCanonical};
 use vortex_btrblocks::BtrBlocksCompressor;
 use vortex_buffer::buffer;
 use vortex_dtype::DType::Utf8;
 use vortex_dtype::Nullability;
+use vortex_error::{VortexResult, vortex_bail};
+use vortex_mask::Mask;
 
 fn main() -> anyhow::Result<()> {
-    let key = buffer![0u64, 1, 2, 3].into_array();
+    let key = buffer![3u64, 1, 5, 7].into_array();
 
     // NB: u16 here is the max length of each token list.
     let tokens = ListArray::from_iter_slow::<u16, _>(
@@ -18,18 +20,56 @@ fn main() -> anyhow::Result<()> {
     )
     .unwrap();
 
-    let structs = StructArray::try_new(
-        ["key".into(), "tokens".into()].into(),
-        vec![key, tokens],
-        4,
-        Validity::NonNullable,
-    )?;
+    let compressed_tokens = BtrBlocksCompressor::compress(&BtrBlocksCompressor, &tokens)?;
 
-    println!(">>> {structs:#?}");
+    let result_array = key_having_token(&key, &compressed_tokens, "foo")?;
 
-    let compressed_structs = BtrBlocksCompressor::compress(&BtrBlocksCompressor, &structs)?;
+    let result = result_array
+        .to_primitive()?
+        .buffer::<u64>()
+        .into_iter()
+        .collect::<Vec<_>>();
 
-    println!(">>> {compressed_structs:#?}");
+    println!(">>> {result:?}");
 
     Ok(())
+}
+
+///
+/// Given a list of (primary) keys and a ListArray<Utf8> of tokens, returns the keys which contain
+/// the given token in their lists.
+///
+fn key_having_token(key: &dyn Array, tokens: &dyn Array, token: &str) -> VortexResult<ArrayRef> {
+    if key.len() != tokens.len() {
+        vortex_bail!("Keys and tokens must have equal lengths.");
+    }
+
+    // TODO: `to_list` seems to be related to decoding, which we don't want to do.
+    let tokens = tokens.to_list()?;
+
+    // Find the matching token offsets.
+    let matching_tokens = compute::compare(
+        tokens.elements(),
+        &ConstantArray::new(token, tokens.elements().len()),
+        compute::Operator::Eq,
+    )?;
+
+    // For each matching token, determine the index of the associated offset.
+    let mut key_mask = BooleanBufferBuilder::new(key.len());
+    key_mask.resize(key.len());
+    for token_offset in matching_tokens.to_bool()?.boolean_buffer().set_indices() {
+        let search_result = compute::search_sorted_usize(
+            tokens.offsets(),
+            token_offset,
+            compute::SearchSortedSide::Left,
+        )?;
+        if let compute::SearchResult::Found(absolute_offset) = search_result {
+            key_mask.set_bit(absolute_offset, true);
+        }
+    }
+
+    // Filter out the matched keys.
+    let matching_keys = compute::filter(key, &Mask::from_buffer(key_mask.into()))?;
+
+    Ok(matching_keys)
 }
