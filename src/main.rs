092a37307e9e44372509f384770dd1dc4bdc5a2e
Stu Hood <stuhood@gmail.com>
2025-05-02 21:40:53 -0700

    Vortex indexing.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,31 @@
 use std::collections::HashSet;
-use std::path::PathBuf;
-use std::sync::Arc;
+use std::path::{Path, PathBuf};
 
-use tempfile::TempDir;
+use clap::Parser;
 use tokio::fs::OpenOptions;
 
-use vortex_array::arrays::{BooleanBufferBuilder, ConstantArray, ListArray, StructArray};
+use vortex_array::arrays::{BooleanBufferBuilder, ConstantArray, StructArray};
+use vortex_array::builders::{ArrayBuilderExt, builder_with_capacity};
 use vortex_array::compute;
 use vortex_array::stream::ArrayStreamArrayExt;
-use vortex_array::{Array, ArrayRef, IntoArray, ToCanonical};
-use vortex_btrblocks::BtrBlocksCompressor;
-use vortex_buffer::buffer;
+use vortex_array::validity::Validity;
+use vortex_array::{Array, ArrayRef, ToCanonical};
 use vortex_dtype::{DType, Nullability};
 use vortex_error::{VortexResult, vortex_bail};
 use vortex_file::VortexWriteOptions;
 use vortex_mask::Mask;
 
+#[derive(Parser, Debug)]
+struct Cli {
+    path: PathBuf,
+    buckets: u16
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let key = buffer![3u64, 1, 5, 7].into_array();
+    let cli = Cli::parse();
 
-    // NB: u16 here is the max length of each token list.
-    let tokens = ListArray::from_iter_slow::<u16, _>(
-        vec![vec!["foo", "bar"], vec![], vec!["baz", "bar"], vec!["foo"]],
-        Arc::new(DType::Utf8(Nullability::NonNullable).into()),
-    )
-    .unwrap();
-
-    let compressed_tokens = BtrBlocksCompressor::compress(&BtrBlocksCompressor, &tokens)?;
-
-    let result_array = key_having_token(&key, &compressed_tokens, "foo")?;
-
-    let result = result_array
-        .to_primitive()?
-        .buffer::<u64>()
-        .into_iter()
-        .collect::<Vec<_>>();
-
-    println!(">>> {result:?}");
+    vortex_index(&cli.path, cli.buckets).await?;
 
     Ok(())
 }
@@ -82,17 +70,20 @@ fn key_having_token(key: &dyn Array, tokens: &dyn Array, token: &str) -> VortexR
 }
 
 ///
-/// Given a non-unique set of sample tokens from a dataset, select `pivot_count` pivot values which
+/// Given a non-unique set of sample tokens from a dataset, select `pivot_count` bucket values which
 /// will roughly equally divide the sample.
 ///
-fn select_pivots_from(mut sample_tokens: Vec<String>, pivot_count: u16) -> Vec<String> {
+fn select_buckets_from(mut sample_tokens: Vec<String>, pivot_count: u16) -> Vec<String> {
     assert!(!sample_tokens.is_empty());
     sample_tokens.sort_unstable();
 
     let bucket_width = sample_tokens.len() as f64 / pivot_count as f64;
-    (0..pivot_count)
+    let mut buckets = (0..pivot_count)
         .map(|idx| sample_tokens[(idx as f64 * bucket_width).floor() as usize].clone())
-        .collect()
+        .collect::<Vec<_>>();
+    // TODO: Duplicates represent a very good opportunity for single-valued buckets.
+    buckets.dedup();
+    buckets
 }
 
 fn tokenize(document: &str) -> HashSet<String> {
@@ -104,35 +95,77 @@ fn tokenize(document: &str) -> HashSet<String> {
         .collect()
 }
 
-fn documents() -> impl Iterator<Item = &'static str> {
-    include_str!("./all_the_henries.txt").lines()
+fn documents() -> impl Iterator<Item = Document> {
+    include_str!("./all_the_henries.txt").lines().map(tokenize)
 }
 
-type Document = (usize, HashSet<String>);
+type Document = HashSet<String>;
 
-async fn vortex_index(
-    temp_dir: TempDir,
-    document_sample: impl Iterator<Item = Document>,
-    documents: impl Iterator<Item = Document>,
-) -> anyhow::Result<PathBuf> {
-    let st = StructArray::try_new(todo!(), todo!(), todo!(), todo!())?;
+async fn vortex_index(path: &Path, bucket_count: u16) -> anyhow::Result<()> {
+    // TODO: Support single-valued buckets.
+    let buckets = select_buckets_from(
+        documents()
+            .take(1000)
+            .map(|doc| doc.into_iter())
+            .flatten()
+            .collect(),
+        bucket_count,
+    );
 
-    vortex_index_array(temp_dir, st.into_array()).await
+    let mut builders = buckets
+        .iter()
+        .map(|_| {
+            builder_with_capacity(
+                &DType::List(
+                    DType::Utf8(Nullability::NonNullable).into(),
+                    Nullability::NonNullable,
+                )
+                .into(),
+                128,
+            )
+        })
+        .collect::<Vec<_>>();
+    let mut entries_to_append: Vec<Vec<String>> = buckets.iter().map(|_| Vec::new()).collect();
+    let mut doc_count = 0;
+    for document in documents() {
+        // Group the tokens by the bucket that they will be appended to.
+        for token in document {
+            let idx = match buckets.binary_search(&token) {
+                Ok(idx) => idx,
+                Err(idx) => idx - 1,
+            };
+            entries_to_append[idx].push(token);
+        }
+        // Drain all buckets into the builders. Many of them will be empty, and that is ok.
+        for (idx, entries) in entries_to_append.iter_mut().enumerate() {
+            builders[idx].append_scalar(&entries.drain(..).collect::<Vec<_>>().into())?;
+        }
+        doc_count += 1;
+    }
+
+    let st = StructArray::try_new(
+        buckets.into_iter().map(|s| s.into()).collect(),
+        builders.into_iter().map(|mut b| b.finish()).collect(),
+        doc_count,
+        Validity::NonNullable,
+    )?;
+
+    vortex_index_array(path, st.into_array()).await?;
+    println!(">>> created {path:?}, with {doc_count} documents in {bucket_count} buckets");
+    Ok(())
 }
 
-async fn vortex_index_array(temp_dir: TempDir, array: ArrayRef) -> anyhow::Result<PathBuf> {
-    let filepath = temp_dir.path().join("a.vortex");
-
+async fn vortex_index_array(path: &Path, array: ArrayRef) -> anyhow::Result<()> {
     let f = OpenOptions::new()
         .write(true)
         .truncate(true)
         .create(true)
-        .open(&filepath)
+        .open(&path)
         .await?;
 
     VortexWriteOptions::default()
         .write(f, array.to_array_stream())
         .await?;
 
-    Ok(filepath)
+    Ok(())
 }
