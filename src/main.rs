6bfff7fe77be11211791b41433e6637865b99275
Stu Hood <stuhood@gmail.com>
2025-05-02 23:01:32 -0700

    Implement single-valued buckets.

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use vortex_mask::Mask;
 #[derive(Parser, Debug)]
 struct Cli {
     path: PathBuf,
-    buckets: u16
+    buckets: u16,
 }
 
 #[tokio::main]
@@ -70,20 +70,55 @@ fn key_having_token(key: &dyn Array, tokens: &dyn Array, token: &str) -> VortexR
 }
 
 ///
-/// Given a non-unique set of sample tokens from a dataset, select `pivot_count` bucket values which
+/// Given a non-unique sample of tokens from a dataset, select `pivot_count` bucket values which
 /// will roughly equally divide the sample.
 ///
-fn select_buckets_from(mut sample_tokens: Vec<String>, pivot_count: u16) -> Vec<String> {
+fn select_buckets_from(
+    mut sample_tokens: Vec<String>,
+    pivot_count: u16,
+) -> Vec<(String, BucketType)> {
     assert!(!sample_tokens.is_empty());
+    assert!(pivot_count > 0);
     sample_tokens.sort_unstable();
 
     let bucket_width = sample_tokens.len() as f64 / pivot_count as f64;
-    let mut buckets = (0..pivot_count)
-        .map(|idx| sample_tokens[(idx as f64 * bucket_width).floor() as usize].clone())
-        .collect::<Vec<_>>();
-    // TODO: Duplicates represent a very good opportunity for single-valued buckets.
-    buckets.dedup();
+    let mut candidates = (0..pivot_count)
+        .map(|idx| sample_tokens[(idx as f64 * bucket_width).floor() as usize].clone());
+
+    let mut previous_candidate = candidates.next().unwrap();
+    let mut single_value_emitted = false;
+    let mut buckets = Vec::with_capacity(pivot_count as usize);
+    for candidate in candidates {
+        if candidate != previous_candidate {
+            // Emit the previous candidate as a multi-valued bucket.
+            buckets.push((previous_candidate, BucketType::Multi));
+            single_value_emitted = false;
+        } else if !single_value_emitted {
+            // Emit the previous candidate as a single-valued bucket.
+            buckets.push((previous_candidate, BucketType::Single));
+            single_value_emitted = true;
+        }
+        previous_candidate = candidate;
+    }
+    // Finally, cap with a multi-valued bucket.
+    buckets.push((previous_candidate, BucketType::Multi));
     buckets
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+enum BucketType {
+    Single,
+    Multi,
+}
+
+impl BucketType {
+    fn column_name(&self, mut token: String) -> String {
+        match self {
+            BucketType::Single => token.push_str(":s"),
+            BucketType::Multi => token.push_str(":m"),
+        }
+        token
+    }
 }
 
 fn tokenize(document: &str) -> HashSet<String> {
@@ -101,28 +136,31 @@ fn documents() -> impl Iterator<Item = Document> {
 
 type Document = HashSet<String>;
 
-async fn vortex_index(path: &Path, bucket_count: u16) -> anyhow::Result<()> {
-    // TODO: Support single-valued buckets.
+async fn vortex_index(path: &Path, buckets: u16) -> anyhow::Result<()> {
     let buckets = select_buckets_from(
         documents()
             .take(1000)
             .map(|doc| doc.into_iter())
             .flatten()
             .collect(),
-        bucket_count,
+        buckets,
     );
+    let bucket_count = buckets.len();
 
     let mut builders = buckets
         .iter()
-        .map(|_| {
-            builder_with_capacity(
+        .map(|(_, btype)| match btype {
+            BucketType::Single => {
+                builder_with_capacity(&DType::Bool(Nullability::NonNullable).into(), 128)
+            }
+            BucketType::Multi => builder_with_capacity(
                 &DType::List(
                     DType::Utf8(Nullability::NonNullable).into(),
                     Nullability::NonNullable,
                 )
                 .into(),
                 128,
-            )
+            ),
         })
         .collect::<Vec<_>>();
     let mut entries_to_append: Vec<Vec<String>> = buckets.iter().map(|_| Vec::new()).collect();
@@ -130,21 +168,37 @@ async fn vortex_index(path: &Path, bucket_count: u16) -> anyhow::Result<()> {
     for document in documents() {
         // Group the tokens by the bucket that they will be appended to.
         for token in document {
-            let idx = match buckets.binary_search(&token) {
+            let idx = match buckets
+                .binary_search_by_key(&(&token, &BucketType::Single), |(token, btype)| {
+                    (token, btype)
+                }) {
                 Ok(idx) => idx,
+                Err(idx) if idx == 0 => 0,
                 Err(idx) => idx - 1,
             };
             entries_to_append[idx].push(token);
         }
         // Drain all buckets into the builders. Many of them will be empty, and that is ok.
         for (idx, entries) in entries_to_append.iter_mut().enumerate() {
-            builders[idx].append_scalar(&entries.drain(..).collect::<Vec<_>>().into())?;
+            match buckets[idx].1 {
+                BucketType::Single => {
+                    let set = !entries.is_empty();
+                    builders[idx].append_scalar(&set.into())?;
+                    entries.clear();
+                }
+                BucketType::Multi => {
+                    builders[idx].append_scalar(&entries.drain(..).collect::<Vec<_>>().into())?
+                }
+            }
         }
         doc_count += 1;
     }
 
     let st = StructArray::try_new(
-        buckets.into_iter().map(|s| s.into()).collect(),
+        buckets
+            .into_iter()
+            .map(|(t, btype)| btype.column_name(t).into())
+            .collect(),
         builders.into_iter().map(|mut b| b.finish()).collect(),
         doc_count,
         Validity::NonNullable,
